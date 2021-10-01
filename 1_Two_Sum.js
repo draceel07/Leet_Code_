137123a2388aa62a8b327ea2fdefbdc26bc348ae
draceel07 <41519955+draceel07@users.noreply.github.com>
2021-10-01 14:18:24 +0530

    Update 1_Two_Sum.js

--- 1_Two_Sum.js
+++ 1_Two_Sum.js
@@ -36,9 +36,20 @@
  * @return {number[]}
  */
 
+//SOLUTION
+const twoSum = (nums, target) => {
+	let result = [];
 
- const twoSum = (nums, target)  => {};
-
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[i] + arr[j] === target) {
+				result.push(i);
+				result.push(j);
+			}
+		}
+	}
+	return result;
+}
 
  console.log(twoSum([2,7,11,15],9))
  console.log(twoSum([2,3,1,11],14))
